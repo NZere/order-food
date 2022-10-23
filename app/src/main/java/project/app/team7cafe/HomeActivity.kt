1713ddda52639f967ec51dc3038bf31a01ddb3ef
Naz3ere <56907794+Naz3ere@users.noreply.github.com>
2022-10-23 19:37:55 +0600

    updated home, added reserve 1 , reserve 2

--- app/src/main/java/project/app/team7cafe/HomeActivity.kt
+++ app/src/main/java/project/app/team7cafe/HomeActivity.kt
@@ -1,7 +1,9 @@
 package project.app.team7cafe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
@@ -11,46 +13,18 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 
 class HomeActivity : AppCompatActivity() {
-
-    private lateinit var auth: FirebaseAuth
-
-    val database = FirebaseDatabase.getInstance()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        auth = Firebase.auth
-
-
-        val email = findViewById<TextView>(R.id.email_text)
-        email.setText(auth.currentUser?.email.toString())
-
-//        val tables = database.getReference("Table")
-//
-//        var hashMap: HashMap<String, Any?> = HashMap()
-//        var table_id ="1"
-//        hashMap["table_id"]= table_id
-//        hashMap["seats_number"]=4
-//        hashMap["is_reserved"]=false
-//
-//
-//        tables.child(table_id!!).setValue(hashMap).addOnSuccessListener {
-//            Toast.makeText(baseContext, "Saved successfully",
-//                Toast.LENGTH_SHORT).show()
-//
-//        }.addOnFailureListener(){
-//            Toast.makeText(baseContext, "Failed in saving",
-//                Toast.LENGTH_SHORT).show()
-//        }
-
-
-
-        Toast.makeText(baseContext, auth.currentUser.toString(),
-            Toast.LENGTH_SHORT).show()
-
-        Toast.makeText(baseContext, auth.currentUser?.email.toString(),
-            Toast.LENGTH_SHORT).show()
-
+        val reserveBtn = findViewById<Button>(R.id.button_tut)
+        val takeBtn = findViewById<Button>(R.id.button_tam)
+        reserveBtn.setOnClickListener {
+            val intent = Intent(this, Reserve1Activity::class.java)
+            startActivity(intent)
+        }
+        takeBtn.setOnClickListener {
+            val intent = Intent(this, Reserve1Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/project/app/team7cafe/Reserve1Activity.kt
@@ -0,0 +1,11 @@
+package project.app.team7cafe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reserve1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reserve1)
+    }
+}
