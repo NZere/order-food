07799a11d645eff61609a280c7cba2e3c16b62d3
Naz3ere <56907794+Naz3ere@users.noreply.github.com>
2022-10-23 23:32:42 +0600

    norm version

--- app/src/main/java/project/app/team7cafe/Model/Table.kt
+++ app/src/main/java/project/app/team7cafe/Model/Table.kt
@@ -8,12 +8,5 @@ class Table {
     var seats_number: Int=0
     var table_id:Int=0
 
-    @Exclude
-    fun toMap(): Map<String, Any?>{
-        return mapOf("is_reserved" to is_reserved,
-            "seats_number" to seats_number,
-            "table_id" to table_id
-        )
-    }
 
 }
