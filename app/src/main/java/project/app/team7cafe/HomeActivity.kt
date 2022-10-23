6394864a17e75fde4d9bc4fa84c23ca1780add3a
Naz3ere <56907794+Naz3ere@users.noreply.github.com>
2022-10-23 18:47:51 +0600

    updated

--- app/src/main/java/project/app/team7cafe/HomeActivity.kt
+++ app/src/main/java/project/app/team7cafe/HomeActivity.kt
@@ -7,11 +7,14 @@ import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+
+    val database = FirebaseDatabase.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,24 @@ class HomeActivity : AppCompatActivity() {
 
         val email = findViewById<TextView>(R.id.email_text)
         email.setText(auth.currentUser?.email.toString())
+
+//        val tables = database.getReference("Table")
+//
+//        var hashMap: HashMap<String, Any?> = HashMap()
+//        var table_id ="1"
+//        hashMap["table_id"]= table_id
+//        hashMap["seats_number"]=4
+//        hashMap["is_reserved"]=false
+//
+//
+//        tables.child(table_id!!).setValue(hashMap).addOnSuccessListener {
+//            Toast.makeText(baseContext, "Saved successfully",
+//                Toast.LENGTH_SHORT).show()
+//
+//        }.addOnFailureListener(){
+//            Toast.makeText(baseContext, "Failed in saving",
+//                Toast.LENGTH_SHORT).show()
+//        }
 
 
 
