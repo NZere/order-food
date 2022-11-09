bacf7d2960f31ac4652e8fa8a4035224422682fb
Naz3ere <56907794+Naz3ere@users.noreply.github.com>
2022-11-09 17:54:38 +0600

    updated

--- app/src/main/java/project/app/team7cafe/ViewHolder/MenuViewHolder.kt
+++ app/src/main/java/project/app/team7cafe/ViewHolder/MenuViewHolder.kt
@@ -1,11 +1,27 @@
 package project.app.team7cafe.ViewHolder
 
 import android.view.View
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import android.widget.TextView
+import project.app.team7cafe.Interface.ItemClickListener
+import project.app.team7cafe.R
 
 class MenuViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-    override fun onClick(v: View?) {
+    private lateinit var itemClickListener: ItemClickListener
+    var txtMenuName: TextView
+    var imageView: ImageView? = null
 
+    init {
+        txtMenuName = itemView.findViewById(R.id.menu_name)
+        imageView=itemView.findViewById(R.id.menu_image)
     }
 
+    fun setItemClickListener(itemClickListener: ItemClickListener){
+        this.itemClickListener=itemClickListener
+    }
+
+    override fun onClick(v: View) {
+        itemClickListener.onClick(v, adapterPosition, false)
+    }
 }
